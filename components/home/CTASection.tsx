a52b989bfa485b1141470232e8fdc3ed7390f0cd
Rishiraj Bhattacharjee <rishirajbhattacharjee974@gmail.com>
2025-03-26 19:29:24 +0530

    Built: Landing Page

--- components/home/CTASection.tsx
+++ components/home/CTASection.tsx
@@ -8,9 +8,11 @@ const CTASection = () => {
     <section className="bg-gray-50 py-12">
       <div className="py-12 lg:py-24 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
-          <div>
-            <h2>Ready to Save Hours of Reading Time?</h2>
-            <p>
+          <div className="space-y-2">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+              Ready to Save Hours of Reading Time?
+            </h2>
+            <p className="mx-auto max-w-2xl text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
               Transform lengthy documents into clear, actionable insights with
               our AI-powered summarizer.
             </p>
@@ -18,10 +20,14 @@ const CTASection = () => {
           <div className="flex flex-col gap-2 min-[400px]:flex-row justify-center">
             <div>
               <Button
+                size="lg"
                 variant={"link"}
                 className="flex items-center justify-center w-full min-[400px]:w-auto bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 hover:text-white text-white transition-all duration-300"
               >
-                <Link href="/#pricing">
+                <Link
+                  href="/#pricing"
+                  className="flex items-center justify-center no-underline px-6 py-6"
+                >
                   Get Started{" "}
                   <ArrowRight className="ml-2 h-4 w-4 animate-pulse" />
                 </Link>
